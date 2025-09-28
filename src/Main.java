c36891425cb4d456b8816c1f001bfa8dbff12ef6
cherep121 <antonchistyuhin@gmail.com>
2025-09-28 15:29:55 +0700

    feat: implement class Balance and Bell

--- src/Main.java
+++ src/Main.java
@@ -66,6 +66,94 @@ public class Main{
 
 
 
+    public static void Task2() {
+        System.out.println("\n~~~~ Задание №2 ~~~~");
+
+        Scanner scanner = new Scanner(System.in);
+        Balance balance = new Balance();
+        boolean run = true;
+
+        while (run){
+            System.out.println("\n$$$$ Меню весов $$$$");
+            System.out.println("(1) ---- Добавить вес на левую чашу");
+            System.out.println("(2) ---- Добавить вес на правую чашу");
+            System.out.println("(3) ---- Результат");
+            System.out.println("(4) ---- Обнулить весы");
+            System.out.println("(5) ---- Вернутся в главное меню\n");
+
+            int choice = scanner.nextInt();
+
+            switch (choice){
+                case 1:
+                    System.out.print("Введите вес для левой чаши: ");
+                    int leftWeight = scanner.nextInt();
+                    balance.addLeft(leftWeight);
+                    Enter(scanner);
+                    break;
+                case 2:
+                    System.out.print("Введите вес для правой чаши: ");
+                    int rightWeight = scanner.nextInt();
+                    balance.addRight(rightWeight);
+                    Enter(scanner);
+                    break;
+                case 3:
+                    balance.result();
+                    Enter(scanner);
+                    break;
+                case 4:
+                    balance.reset();
+                    Enter(scanner);
+                    break;
+                case 5:
+                    run = false;
+                    System.out.println("//// Возврат в главное меню ////");
+                    Enter(scanner);
+                    break;
+                default:
+                    System.out.println("!!!! неверный ввод, попробуйте снова !!!!");
+                    Enter(scanner);
+            }
+        }
+    }
+
+
+
+    public static void Task3(){
+        System.out.println("\n~~~~ Лабораторная работа №3 ~~~~");
+
+        Scanner scanner = new Scanner(System.in);
+        Bell bell = new Bell();
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n$$$$ Меню колокола $$$$");
+            System.out.println("(1) ---- Позвонить");
+            System.out.println("(2) ---- Сбросить");
+            System.out.println("(3) ---- Вернуться в главное меню");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    bell.sound();
+                    Enter(scanner);
+                    break;
+                case 2:
+                    bell.reset();
+                    Enter(scanner);
+                    break;
+                case 3:
+                    running = false;
+                    System.out.println("//// Возврат в главное меню ////");
+                    Enter(scanner);
+                    break;
+                default:
+                    System.out.println("!!!! Неверный ввод попробуйте снова !!!!");
+            }
+        }
+    }
+
+
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
@@ -80,10 +168,10 @@ public class Main{
                     Task1();
                     break;
                 case 2:
-                    //Task2();
+                    Task2();
                     break;
                 case 3:
-                    //Task3();
+                    Task3();
                     break;
                 case 4:
                     //Task4();
