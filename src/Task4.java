0c03283839f41be87226224576196eae588bda01
cherep121 <antonchistyuhin@gmail.com>
2025-10-07 19:13:01 +0700

    feet(lab3): completing task 4

---
+++ src/Task4.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Task4 {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void execute() {
+        System.out.println("\n@@@@ ЗАДАНИЕ №4 @@@@");
+
+        System.out.println("Введите текст на английском языке:");
+        scanner.nextLine();
+        String text = scanner.nextLine();
+
+        Map<String, Integer> wordFrequency = countWordFrequency(text);
+
+        System.out.println("\nЧастота встречаемости слов:");
+        for (Map.Entry<String, Integer> entry : wordFrequency.entrySet()) {
+            System.out.printf("'%s': %d раз(а)%n", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private Map<String, Integer> countWordFrequency(String text) {
+        Map<String, Integer> frequencyMap = new HashMap<>();
+
+        String[] words = text.toLowerCase().split("\\W+");
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        return frequencyMap;
+    }
+}
