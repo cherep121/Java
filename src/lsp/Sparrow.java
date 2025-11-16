0ed6e436d080aa9a1a53513d9d09d54880a1ba73
cherep121 <antonchistyuhin@gmail.com>
2025-11-16 17:46:22 +0700

    feat(lab8): creating interface for Fax, Printer, Scaner

--- src/lsp/Sparrow.java
+++ src/lsp/Sparrow.java
@@ -3,7 +3,7 @@ package lsp;
 public class Sparrow extends FlyingBird {
     @Override
     public void eat() {
-        System.out.println("Чирик-чирик, клюв сенечки, чирик-чирик");
+        System.out.println("Чирик-чирик, клюв семечки, чирик-чирик");
     }
 
     @Override
